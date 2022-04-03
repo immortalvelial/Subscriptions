e5d931a8970326fdefb61c98ecd405684026f7aa
immortalprogrammist <immortalvelial@icloud.com>
2022-04-03 17:05:57 +0200

    Initial Commit

---
+++ Subscriptions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subscriptions
+//
+//  Created by Nikita on 03.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
